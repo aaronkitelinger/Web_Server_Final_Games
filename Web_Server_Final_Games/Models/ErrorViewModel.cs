d10c18fed6e33b625377b3866f391b30769db8ab
Aaron Kitelinger <kiteliad@mail.uc.edu>
2022-11-27 19:44:54 -0500

    Add project files.

---
+++ Web_Server_Final_Games/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_Server_Final_Games.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
